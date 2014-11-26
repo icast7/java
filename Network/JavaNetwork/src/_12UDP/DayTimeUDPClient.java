4f81621d66456b2ba95b03189cf2b251335a1c4b
ikaz <icastillejos@gmail.com>
2014-11-25 16:08:53 -0800

    Added server

--- Network/JavaNetwork/src/_12UDP/DayTimeUDPClient.java
+++ Network/JavaNetwork/src/_12UDP/DayTimeUDPClient.java
@@ -6,18 +6,19 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 public class DayTimeUDPClient {
-	private final static int PORT = 13;
-	private static final String HOSTNAME =  "time.nist.gov";
+	private final static int PORT = 5000;
+	//	private static final String HOSTNAME =  "time.nist.gov";
+	private static final String HOSTNAME =  "localhost";
 	
 	public static void main(String[] args){
-		try (DatagramSocket socket = new DatagramSocket(5000)){
-			socket.setSoTimeout(60000);
+		try (DatagramSocket socket = new DatagramSocket(0)){
+			socket.setSoTimeout(15000);
 			InetAddress host = InetAddress.getByName(HOSTNAME);
 			//byte[] reqData = new byte[1];
 			//reqData[0] =  (byte)0;
 			DatagramPacket request = new DatagramPacket(new byte[1], 1, host, PORT);
 			DatagramPacket response= new DatagramPacket(new byte[1024], 1024);
-			System.out.println(request.getAddress() +":::"+ request.getData() +"::::" + request.getPort());
+			System.out.println(request.getAddress() +":" + request.getPort());
 			socket.send(request);
 			socket.receive(response);
 			String result = new String(response.getData(), 0, response.getLength(), "US-ASCII");
